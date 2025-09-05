046807cdf60ff414df5ab54ae7fab922a256dbab
igorsnf <igorsnf123@hotmail.com>
2025-09-04 23:07:42 -0300

    atualização projeto

---
+++ test/gerenciarUsuarios.test.js
@@ -0,0 +1,17 @@
+const{ retornarUsuarios , adicionarNovoUsuario } = require ('../src/gerenciarUsuarios');
+const {expect} = require ('chai');
+
+describe('Testar as Funções de Gestçao de Usuários',function(){
+    it('Validar que posso adicionar um novo nome de usuário na lista', function(){
+        adicionarNovoUsuario ('Maria');
+        const listaDeUsuarios = retornarUsuarios();
+        expect (listaDeUsuarios.at(-1)).to.equal('Maria');
+
+
+
+    })
+
+
+
+
+})
